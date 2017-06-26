db9bd132d0012bac420d2bd6472e1153a27af893
michael <18alexanderm117@tongji.edu.cn>
2017-06-26 10:45:36 +0800

    added make dir

--- misc.py
+++ misc.py
@@ -3,3 +3,9 @@ import numpy as np
 
 def cal_num_parameters():
     return np.sum(list(map(lambda tv: np.prod(tv.get_shape().as_list()), tf.trainable_variables())))
+
+
+def delete_and_make_dir(dir):
+    if tf.gfile.Exists(dir):
+        tf.gfile.DeleteRecursively(dir)
+    tf.gfile.MakeDirs(dir)
