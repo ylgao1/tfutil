1765344a417828b8ff7f9cb534d64af03f93e9f3
Michael Chuai <18alexanderm117@tongji.edu.cn>
2017-12-29 13:28:39 +0800

    m

--- metrics.py
+++ metrics.py
@@ -83,17 +83,21 @@ def rankdata(a, method='average', scope='rankdata'):
         _, sorter = tf.nn.top_k(-arr, arr.get_shape()[-1])
         inv = tf.invert_permutation(sorter)
         if method == 'ordinal':
-            return inv + 1
-        arr = tf.gather(arr, sorter)
-        obs = tf.cast(tf.not_equal(arr[1:], arr[:-1]), dtype=tf.int32)
-        obs = tf.concat([[1], obs], axis=0)
-        dense = tf.gather(tf.cumsum(obs), inv)
-        if method == 'dense':
-            return dense
-        count = tf.reshape(tf.where(tf.not_equal(obs, tf.zeros_like(obs))), [-1])
-        count = tf.concat([count, obs.get_shape()], axis=0)
-        if method == 'max':
-            return tf.gather(count, dense)
-        if method == 'min':
-            return tf.gather(count, dense - 1) + 1
-        return (tf.gather(count, dense) + tf.gather(count, dense - 1) + 1) / 2
+            res = inv + 1
+        else:
+            arr = tf.gather(arr, sorter)
+            obs = tf.cast(tf.not_equal(arr[1:], arr[:-1]), dtype=tf.int32)
+            obs = tf.concat([[1], obs], axis=0)
+            dense = tf.gather(tf.cumsum(obs), inv)
+            if method == 'dense':
+                res = dense
+            else:
+                count = tf.reshape(tf.where(tf.not_equal(obs, tf.zeros_like(obs))), [-1])
+                count = tf.concat([count, obs.get_shape()], axis=0)
+                if method == 'max':
+                    res = tf.gather(count, dense)
+                elif method == 'min':
+                    res = tf.gather(count, dense - 1) + 1
+                else:
+                    res = (tf.gather(count, dense) + tf.gather(count, dense - 1) + 1) / 2
+        return tf.cast(res, tf.float32)
