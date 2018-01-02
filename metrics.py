41fa8daa5fc3feec7281a0eac66dfe615e464b65
Michael Chuai <18alexanderm117@tongji.edu.cn>
2018-01-02 11:43:22 +0800

    m

--- metrics.py
+++ metrics.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from functools import wraps
 import numpy as np
 from tensorflow.python.ops.metrics_impl import _create_local, _remove_squeezable_dimensions
+from tensorflow.python.ops import confusion_matrix
 
 
 def add_reset_op(sc=None):
@@ -13,7 +14,11 @@ def add_reset_op(sc=None):
                 vars = tf.contrib.framework.get_variables(
                     scope.original_name_scope, collection=tf.GraphKeys.LOCAL_VARIABLES)
                 reset_op = tf.variables_initializer(vars)
-            return metric_op, update_op, reset_op
+                with tf.control_dependencies([reset_op]):
+                    op1 = tf.identity(update_op)
+                with tf.control_dependencies([op1]):
+                    op2 = tf.identity(metric_op)
+            return metric_op, update_op, reset_op, op1, op2
 
         return wrapper
 
@@ -50,6 +55,83 @@ def metrics_mean_per_class_accuracy(labels, logits, num_classes, weights=None,
     predictions = tf.argmax(logits, axis=-1)
     return _metrics_mean_per_class_accuracy(labels, predictions, num_classes, weights,
                                             metrics_collections, updates_collections, name)
+
+
+def _streaming_confusion_matrix(labels, predictions, num_classes, weights=None,
+                                metrics_collections=None,
+                                updates_collections=None,
+                                name=None):
+    """Calculate a streaming confusion matrix.
+
+    Calculates a confusion matrix. For estimation over a stream of data,
+    the function creates an  `update_op` operation.
+
+    Args:
+      labels: A `Tensor` of ground truth labels with shape [batch size] and of
+        type `int32` or `int64`. The tensor will be flattened if its rank > 1.
+      predictions: A `Tensor` of prediction results for semantic labels, whose
+        shape is [batch size] and type `int32` or `int64`. The tensor will be
+        flattened if its rank > 1.
+      num_classes: The possible number of labels the prediction task can
+        have. This value must be provided, since a confusion matrix of
+        dimension = [num_classes, num_classes] will be allocated.
+      weights: Optional `Tensor` whose rank is either 0, or the same rank as
+        `labels`, and must be broadcastable to `labels` (i.e., all dimensions must
+        be either `1`, or the same as the corresponding `labels` dimension).
+
+    Returns:
+      total_cm: A `Tensor` representing the confusion matrix.
+      update_op: An operation that increments the confusion matrix.
+    """
+    # Local variable to accumulate the predictions in the confusion matrix.
+    with tf.variable_scope(name, 'confusion_matrix'):
+        total_cm = _create_local(
+            'total_confusion_matrix',
+            shape=[num_classes, num_classes],
+            dtype=tf.float64)
+
+        # Cast the type to int64 required by confusion_matrix_ops.
+        predictions = tf.to_int64(predictions)
+        labels = tf.to_int64(labels)
+        num_classes = tf.to_int64(num_classes)
+
+        # Flatten the input if its rank > 1.
+        if predictions.get_shape().ndims > 1:
+            predictions = tf.reshape(predictions, [-1])
+
+        if labels.get_shape().ndims > 1:
+            labels = tf.reshape(labels, [-1])
+
+        if (weights is not None) and (weights.get_shape().ndims > 1):
+            weights = tf.reshape(weights, [-1])
+
+        # Accumulate the prediction to current confusion matrix.
+        current_cm = confusion_matrix.confusion_matrix(
+            labels, predictions, num_classes, weights=weights, dtype=tf.float64)
+        update_op = tf.assign_add(total_cm, current_cm)
+        if metrics_collections:
+            tf.add_to_collection(metrics_collections, total_cm)
+        if updates_collections:
+            tf.add_to_collection(updates_collections, update_op)
+        return total_cm, update_op
+
+
+@add_reset_op('metrics')
+def _metrics_streaming_confusion_matrix(labels, predictions, num_classes, weights=None,
+                                        metrics_collections=None, updates_collections=None,
+                                        name=None):
+    return _streaming_confusion_matrix(labels, predictions, num_classes, weights,
+                                       metrics_collections, updates_collections,
+                                       name)
+
+
+def metrics_confusion_matrix(labels, logits, num_classes, weights=None,
+                             metrics_collections=None, updates_collections=None,
+                             name=None):
+    predictions = tf.argmax(logits, axis=-1)
+    return _metrics_streaming_confusion_matrix(labels, predictions, num_classes, weights,
+                                               metrics_collections, updates_collections,
+                                               name)
 
 
 @add_reset_op('metrics')
